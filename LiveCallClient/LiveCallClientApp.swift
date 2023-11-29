d2b431a466b9015cdbcefbe3569b0bfd9ec1ba03
tuanlongn <tuanlongn@gmail.com>
2023-11-29 10:44:18 +0700

    Initial Commit

---
+++ LiveCallClient/LiveCallClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCallClientApp.swift
+//  LiveCallClient
+//
+//  Created by Ngo Tuan Long on 29/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveCallClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
